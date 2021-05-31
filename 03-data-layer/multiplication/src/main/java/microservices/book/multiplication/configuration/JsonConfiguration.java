1e1d95e25096741857ebf3179d0e42a144cffb28
Michael Russo <russo.mail@gmail.com>
2021-05-31 15:57:27 -0400

    Adds comments for config and dependency

--- 03-data-layer/multiplication/src/main/java/microservices/book/multiplication/configuration/JsonConfiguration.java
+++ 03-data-layer/multiplication/src/main/java/microservices/book/multiplication/configuration/JsonConfiguration.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration to handle lazy fetching with JSON serialization.
+ */
 @Configuration
 public class JsonConfiguration {
 
